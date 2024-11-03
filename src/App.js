0840aefaff8139fdbb3d30d05294f78fe4b96c03
Bushra Sayyed <sayyedbushra2001@gmail.com>
2024-11-03 16:25:18 +0530

    features added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-import{
-  BrowserRouter,
-  Routes,
-  Route
-  
-} from "react-router-dom";
+
 function App() {
   return (
     <>
- <BrowserRouter>
+ 
 <Navbar/>
 <div className='container my-3'>
 
-  <Routes>
-   <Route path="/" element={<TextForm />}>
-  </Route>
-  </Routes>
+ <TextForm/>
   
   </div>
 
- </BrowserRouter> 
+ 
   </>
   );
 }
